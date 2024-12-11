15ad9b01bddbc19f56747fc9008721af1d39460b
Karanbir Singh <krnbr@live.in>
2024-12-11 11:17:48 +0530

    added route

--- src/main/java/in/neuw/demo/DemoThreeDotFourApplication.java
+++ src/main/java/in/neuw/demo/DemoThreeDotFourApplication.java
@@ -2,9 +2,17 @@ package in.neuw.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoThreeDotFourApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoThreeDotFourApplication.class, args);
