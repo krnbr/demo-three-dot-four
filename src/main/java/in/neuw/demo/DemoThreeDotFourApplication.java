24cbd2c4a619d08040606d57a281bc7f37955fc5
Karanbir Singh <krnbr@live.in>
2024-12-11 11:16:26 +0530

    init

---
+++ src/main/java/in/neuw/demo/DemoThreeDotFourApplication.java
@@ -0,0 +1,13 @@
+package in.neuw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoThreeDotFourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoThreeDotFourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/neuw/demo/DemoThreeDotFourApplicationTests.java
@@ -0,0 +1,13 @@
+package in.neuw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoThreeDotFourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
